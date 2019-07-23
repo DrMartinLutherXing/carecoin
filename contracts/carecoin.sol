da96df2661885b4ae23649a65a23b5435d3606aa
kf <drmartinlutherchang@gmail.com>
2019-07-23 02:01:45 -0700

    extend ERC20Mintable;
     On branch master
     Changes to be committed:
    	modified:   contracts/carecoin.sol

--- contracts/carecoin.sol
+++ contracts/carecoin.sol
@@ -2,11 +2,4 @@ pragma solidity ^0.5.8;
 
 import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
 
-contract carecoin is ERC20Mintable {
-
-	constructor () public ERC20Mintable()
-	{
-		mint(msg.sender, 0);
-	}
-
-}
+contract carecoin is ERC20Mintable { }
