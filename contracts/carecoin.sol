67e7041a600612ed7b102f80ee823d0686f07060
kf <drmartinlutherchang@gmail.com>
2019-07-23 01:49:53 -0700

    stripped down carecoin extending ERC20Mintable; not initial supply;
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   build/contracts/carecoin.json
    	modified:   contracts/carecoin.sol

--- contracts/carecoin.sol
+++ contracts/carecoin.sol
@@ -1,13 +1,12 @@
 pragma solidity ^0.5.8;
 
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
+import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
 
 contract carecoin is ERC20Mintable {
-	uint private INITIAL_SUPPLY = 10000e18;
 
-	constructor () public ERC20Mintable("carecoin", "CARE", 18)
+	constructor () public ERC20Mintable()
 	{
-		_mint(msg.sender, INITIAL_SUPPLY);
+		mint(msg.sender, 0);
 	}
 
 }
