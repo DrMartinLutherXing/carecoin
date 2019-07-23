6c21569c217fa5c80a65cbb2fb6c27eebc64c41c
kf <drmartinlutherchang@gmail.com>
2019-07-23 01:20:09 -0700

    swith to mintable;
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   contracts/carecoin.sol

--- contracts/carecoin.sol
+++ contracts/carecoin.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.5.8;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
 
-contract carecoin is ERC20Detailed {
+contract carecoin is ERC20Mintable {
 	uint private INITIAL_SUPPLY = 10000e18;
 
-	constructor () public ERC20Detailed("carecoin", "CARE", 18)
+	constructor () public ERC20Mintable("carecoin", "CARE", 18)
 	{
 		_mint(msg.sender, INITIAL_SUPPLY);
 	}
