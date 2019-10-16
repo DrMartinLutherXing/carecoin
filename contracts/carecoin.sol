7754d5cc8b82cf67cdeefba7415d8bc1ea678e99
bubbleboy14 <mario.balibrera@gmail.com>
2019-10-16 15:23:33 -0700

    cc contract update (name, symbol)

--- contracts/carecoin.sol
+++ contracts/carecoin.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.8;
 
 import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
 
-contract carecoin is ERC20Mintable { }
+contract carecoin is ERC20Mintable {
+
+	string private _name;
+	string private _symbol;
+
+	constructor() public {
+		    _name = "carecoin";
+		    _symbol = "CARE";
+	}
+
+	function name() public view returns (string memory) {
+		 return _name;
+	}
+
+	function symbol() public view returns (string memory) {
+		 return _symbol;
+	}
+}
